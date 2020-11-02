2fc505ef80103722d978e3da03e3109b4fee33cf
Balazs Vincze <hello@bvincze.com>
2020-11-02 18:46:00 +0100

    Added a template for Quick tests.

---
+++ Quick Test Spec.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,13 @@
+//___FILEHEADER___
+
+@testable import ___PROJECTNAME___
+import Quick
+import Nimble
+
+final class ___FILEBASENAMEASIDENTIFIER___: QuickSpec {
+    
+    override func spec() {
+        
+    }
+
+}
